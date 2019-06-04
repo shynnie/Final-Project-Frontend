4ad64d694c66ca386bba29d297e0c1f2cceb5f8f
shynnie <hung.le150049@vnuk.edu.vn>
2019-06-04 12:05:22 +0700

    First update frontend

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+	"use strict";
+
+	var window_width 	 = $(window).width(),
+	window_height 		 = window.innerHeight,
+	header_height 		 = $(".default-header").height(),
+	header_height_static = $(".site-header.static").outerHeight(),
+	fitscreen 			 = window_height - header_height;
+
+
+	$(".fullscreen").css("height", window_height)
+    $(".fitscreen").css("height", fitscreen);
+ 
+ // Search Toggle
+    $("#search_input_box").hide();
+    $("#search").on("click", function () {
+        $("#search_input_box").slideToggle();
+        $("#search_input").focus();
+    });
+    $("#close_search").on("click", function () {
+        $('#search_input_box').slideUp(500);
+    });
+});
