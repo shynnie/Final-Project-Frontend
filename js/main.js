1f96b22cb7e98022ec86005a0219f11362069019
shynnie <hung.le150049@vnuk.edu.vn>
2019-06-16 07:39:46 +0700

    Edit Navigation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,51 @@
 
+
+/////////////////////////////////
+
+// TABLE OF CONTENT
+
+////////////////////////////////
+
+// 1. Local storage
+// 10. Search toggle
+
+///////////////////
+
+// For blog page test only
+
+///////////////////
+    // var myJson = '{"menu1":"home.html","menu2":"category.html","menu3":"blog.html","menu4":"contact.html"}';
+    // var myObj = JSON.parse(myJson);
+    // // document.getElementById("menu_item_1").textContent = "home";
+    // // document.getElementById("menu_item_1").setAttribute("href", myObj.menu1);
+    // document.getElementById("menu_item_2").textContent = "category";
+    // document.getElementById("menu_item_2").setAttribute("href", myObj.menu2);
+    // document.getElementById("menu_item_3").textContent = "blog";
+    // document.getElementById("menu_item_3").setAttribute("href", myObj.menu3);
+    // document.getElementById("menu_item_4").textContent = "contact";
+    // document.getElementById("menu_item_4").setAttribute("href", myObj.menu4);
+
+    // var myJson2 = {menu1:"home.html", menu2:"category.html", menu3:"blog.html", menu4:"contact.html"};
+
+    // var myStorage = JSON.stringify(myJson2);
+    // localStorage.setItem('testJSON', myStorage);
+
+    // text = localStorage.getItem('testJSON');
+    // obj = JSON.parse(text);
+    // document.getElementById("menu_item_1").textContent = "home";
+    // document.getElementById("menu_item_1").setAttribute('href', obj.menu1);
+
+//////////////////
+
+
+
+
+// 10. Search toggle
+
+///////////////////
 $(document).ready(function(){
-	"use strict";
-
-// 	var window_width 	 = $(window).width(),
-// 	window_height 		 = window.innerHeight,
-// 	header_height 		 = $(".default-header").height(),
-// 	header_height_static = $(".site-header.static").outerHeight(),
-// 	fitscreen 			 = window_height - header_height;
-
-
-// 	$(".fullscreen").css("height", window_height)
-//     $(".fitscreen").css("height", fitscreen);
- 
+    "use strict";
+    
  // Search Toggle
     $("#search_input_box").hide();
     $("#search").on("click", function () {
@@ -26,3 +60,57 @@ $(document).ready(function(){
 
 });
 
+
+
+$(document).ready(function(){
+    "use strict";
+
+///////////////////
+
+// 1. Local Storage
+
+///////////////////
+
+    // add to cart
+
+    var prdJson = {id:1, img:"img/product5.jpg", name:"Faded SkyBlu Denim Pum", price:"$1500", quantity:"1", category:"pum", available:"In stock", bio:"Last but by no means least on our list of best mechanics toys and tools is this one. Want to teach your children how stuff works? Perhaps its time for a fun introduction to the basics of engineering? This kit will teach them about flying machines, boats, land vehicles, levers, forces, and pulleys."};
+    var singlePrdJson = {id:2, img:"img/product.jpg", name:"Faded SkyBlu Denim Pum", price:"$1500", quantity:"1", total:"$1500"}
+    
+    var myPrdStr = JSON.stringify(prdJson);
+    var mySinglePrdStr = JSON.stringify(singlePrdJson);
+
+    localStorage.setItem('singlePrdJson', mySinglePrdStr);
+    localStorage.setItem('prdJson', myPrdStr);
+
+    var sPrdText = localStorage.getItem('singlePrdJson');
+    var prdText = localStorage.getItem('prdJson');
+
+    var obj = JSON.parse(sPrdText);
+    
+
+    function addToCart() {
+        var newHtml;
+        var html = '<tr><td><div class="media"><div class="d-flex"><img src="%img%"></div><div class="media-body"><p>%name%</p></div></div></td><td><div class="product_count"><input type="text" name="qty" id="sst" maxlength="12" value="%quantity%" title="Quantity:"class="input-text qty"><button onclick="var result = document.getElementById(\'sst\'); var sst = result.value; if( !isNaN( sst )) result.value++;return false;"class="increase items-count" type="button"><i class="lnr lnr-chevron-up"></i></button><button onclick="var result = document.getElementById(\'sst\'); var sst = result.value; if( !isNaN( sst ) &amp;&amp; sst > 0 ) result.value--;return false;"class="reduced items-count" type="button"><i class="lnr lnr-chevron-down"></i></button></div></td><td><h5>%price%</h5></td><td><h5>%total%</h5></td></tr>'
+        
+        // replace detail product
+        newHtml = html.replace('%name%', obj.name);
+        newHtml = newHtml.replace('%quantity%', obj.quantity);
+        newHtml = newHtml.replace('%img%', obj.img);
+        newHtml = newHtml.replace('%price%', obj.price);
+        newHtml = newHtml.replace('%total%', obj.total);
+        
+        $("tbody").prepend(newHtml);
+    }
+    var count = 0;
+    
+    // change checkout_items
+
+    $('.prd-bottom').click(() => {
+        count += 1;
+        $('#checkout_items').text(count);
+    });
+    
+});
+
+
+///////////////////
--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,151 @@
+jQuery(document).ready(function($)
+{
+	"use strict";
 
-var $scrollTo = $('#main_menu'),
-inputElem = $('<input type="text"></input>');
+	/* 
 
-$scrollTo.prepend(inputElem);
-inputElem.css({
-  position: 'absolute',
-  width: '1px',
-  height: $scrollTo.height()
+	Vars and Inits
+
+	*/
+
+	var header = $('.header');
+	var topNav = $('.top_nav')
+	var mainSlider = $('.main_slider');
+	var hamburger = $('.hamburger_container');
+	var menu = $('.hamburger_menu');
+	var menuActive = false;
+	var hamburgerClose = $('.hamburger_close');
+	var fsOverlay = $('.fs_menu_overlay');
+
+	setHeader();
+
+	
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+	// initTimer();
+	// initFavorite();
+	// initFixProductBorder();
+	// initIsotopeFiltering();
+	// initSlider();
+
+	/* 
+
+	Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if(window.innerWidth < 992)
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.css({'top':"0"});
+			}
+			else
+			{
+				header.css({'top':"0"});
+			}
+		}
+		else
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.css({'top':"-50px"});
+			}
+			else
+			{
+				header.css({'top':"0"});
+			}
+		}
+		if(window.innerWidth > 991 && menuActive)
+		{
+			closeMenu();
+		}
+	}
+
+	/* 
+
+	Init Menu
+
+	*/
+
+	function initMenu()
+	{
+		if(hamburger.length)
+		{
+			hamburger.on('click', function()
+			{
+				if(!menuActive)
+				{
+					openMenu();
+				}
+			});
+		}
+
+		if(fsOverlay.length)
+		{
+			fsOverlay.on('click', function()
+			{
+				if(menuActive)
+				{
+					closeMenu();
+				}
+			});
+		}
+
+		if(hamburgerClose.length)
+		{
+			hamburgerClose.on('click', function()
+			{
+				if(menuActive)
+				{
+					closeMenu();
+				}
+			});
+		}
+
+		if($('.menu_item').length)
+		{
+			var items = document.getElementsByClassName('menu_item');
+			var i;
+
+			for(i = 0; i < items.length; i++)
+			{
+				if(items[i].classList.contains("has-children"))
+				{
+					items[i].onclick = function()
+					{
+						this.classList.toggle("active");
+						var panel = this.children[1];
+					    if(panel.style.maxHeight)
+					    {
+					    	panel.style.maxHeight = null;
+					    }
+					    else
+					    {
+					    	panel.style.maxHeight = panel.scrollHeight + "px";
+					    }
+					}
+				}	
+			}
+		}
+	}
+
+	function openMenu()
+	{
+		menu.addClass('active');
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		menu.removeClass('active');
+		menuActive = false;
+	}
 });
-inputElem.focus();
-inputElem.remove();
